94c90910d18276683f9c3b6c6ed1d60dc06ad1cf
HackDream2018 <18569069613@163.com>
2019-05-11 00:06:57 +0800

    121

---
+++ src/test/java/test/TT.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @version v1.0
+ * @author: TianXiang
+ * @description:
+ * @date: 2019/5/4
+ */
+public class TT {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.forEach(item -> {
+             System.out.println(item);
+        });
+         System.out.println("1111");
+    }
+    
+}
