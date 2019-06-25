463f1072f86768edd7c5418c8ef4056bf7f72eaf
HackDream2018 <18569069613@163.com>
2019-06-25 17:40:09 +0800

    6.16

--- src/test/java/test/TT.java
+++ src/test/java/test/TT.java
@@ -8,16 +8,7 @@ package test;
  */
 public class TT {
 
-    public static void main(String[] args) throws Exception {
-       /* Class<Person> clazz = Person.class;
-        Constructor<Person> constructor = clazz.getConstructor(String[].class);
-        String[] strArray = new String[]{"速度", "激情"};
-        constructor.newInstance(new Object[]{strArray});
-        */
-        String[] str = {"1", "2", "3"};
-        Object[] str2 = new Object[]{str};
-        System.out.println(str2.length); //1
-
+    public static void main(String[] args) {
 
     }
 
