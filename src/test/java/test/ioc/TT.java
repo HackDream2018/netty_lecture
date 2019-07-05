bd703e2c15abe0c9f7f000e3ceb0817f2c9eff26
HackDream2018 <18569069613@163.com>
2019-07-06 02:36:57 +0800

    7.6

--- src/test/java/test/ioc/TT.java
+++ src/test/java/test/ioc/TT.java
@@ -11,10 +11,8 @@ public class TT {
     public static void main(String[] args) throws Exception {
         // 对象的创建交给容器来处理, 顺便赋值
         MySpring spring = new MySpring();
-        Animal animal = (Animal) spring.getBean("test.ioc.Animal");
-         System.out.println(animal);
-        /*Person person = (Person) spring.getBean("test.ioc.Person");
-        System.out.println(person);*/
+        Person person = (Person) spring.getBean("test.ioc.Person");
+        System.out.println(person);
 
     }
 
