11fafdf29f2a84b4ed9773f0668002f7b90307e5
HackDream2018 <18569069613@163.com>
2019-08-24 21:42:32 +0800

    8.24

---
+++ src/test/java/test/Animal.java
@@ -0,0 +1,16 @@
+package test;
+
+import lombok.Data;
+
+/**
+ * @version v1.0
+ * @author: TianXiang
+ * @description:
+ * @date: 2019/8/19
+ */
+@Data
+public class Animal {
+
+    private String name;
+
+}
